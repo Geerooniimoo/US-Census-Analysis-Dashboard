aead5abcad0823900ed757ed36bf63e07d1c24dc
Geronimo Perez <geronimo.perez4@gmail.com>
2021-01-28 21:57:00 -0500

    continue working on version 4

--- assets/js/app4.js
+++ assets/js/app4.js
@@ -16,21 +16,21 @@ xText
     .append('text')
     .attr('data-id','income')
     .text('Household Income (median)')
-    .attr('class','aText x inactive')
+    .attr('class','aText x inactive');
     
 xText
     .append('text')
     .attr('y',-25)
     .attr('data-id','age')
     .text('Age (median)')
-    .attr('class','aText x inactive')
+    .attr('class','aText x inactive');
     
 xText
     .append('text')
     .attr('y',-50)
     .attr('data-id','poverty')
     .text('In Poverty (%)')
-    .attr('class','aText x active')
+    .attr('class','aText x active');
 
 var yText = svg.append('g').attr('transform',`translate(20,${height/2})rotate(-90)`);
 
@@ -38,21 +38,41 @@ yText
     .append('text')
     .attr('data-id','obesity')
     .text('Obese (%)')
-    .attr('class','aText y active')
+    .attr('class','aText y active');
     
 yText
     .append('text')
     .attr('y',25)
     .attr('data-id','smokes')
     .text('Smokes (%)')
-    .attr('class','aText y inactive')
+    .attr('class','aText y inactive');
     
 yText
     .append('text')
     .attr('y',50)
     .attr('data-id','poverty')
     .text('Lacks Healthcare (%)')
-    .attr('class','aText y inactive')
+    .attr('class','aText y inactive');
+
+var circles = svg.append('g').attr('class','circles');
+
+var xScale = svg
+    .append('g')
+    .attr('transform',`translate(${.10*width},${.85*height})`)
+    .call(d3.axisBottom(d3.scaleLinear().range([0,.80*width])));
+
+var yScale = svg
+    .append('g')
+    .attr('transform',`translate(${.10*width},${.85*height})`)
+    .transition()
+    .duration(5000);
+
+d3.csv('assets/data/data.csv').then(data => {
+
+    data.forEach(obj => {
+        circles.append('g').attr('class','circle');
+    });
+} );
 
 showData();
 
@@ -61,12 +81,40 @@ function showData() {
     var xSel = d3.selectAll('.x').filter('.active').attr('data-id');
     var ySel = d3.selectAll('.y').filter('.active').attr('data-id');
 
-    console.log(ySel);
-    
     d3.csv('assets/data/data.csv').then(data => {
+        csvData = data;
+        
         var abbr = data.map(obj => obj.abbr)
         var xVal = data.map(obj => +obj[xSel])
         var yVal = data.map(obj => +obj[ySel])
-        console.log(abbr);
+
+        // console.log(xVal.min());
+
+        xScale
+            .call(d3.axisBottom(d3.scaleLinear()
+            .domain([d3.min(xVal),d3.max(xVal)])
+            .range([0,.80*width])))
+
+        d3.selectAll('.circle').each(function(obj, i){
+            d3
+                .select(this)
+                .append('circle')
+                .attr('r',10)
+                .attr('class','stateCircle')
+
+        });        
+
     });
 };
+
+d3.selectAll('.aText').on('click', function () {
+    let sel = d3.select(this);
+    
+    if(sel.attr('class').includes(' x')){
+        d3.selectAll('.x').filter('.active').classed('active',false).classed('inactive',true);
+    } else {
+        d3.selectAll('.y').filter('.active').classed('active',false).classed('inactive',true);
+    };
+    
+    sel.classed('inactive',false).classed('active',true);
+});
